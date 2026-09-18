79016a67e8f1039d747e6a27a2fb10d1becc0054
Bruno <bmelo955@gmail.com>
2026-09-18 16:47:51 -0300

    ci: configura deploy automatico via GitHub Actions e GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     root: 'html',
+    base: '/desenvolvimento-frontend-spa/',
     build: {
         outDir: '../dist',
         emptyOutDir: true
